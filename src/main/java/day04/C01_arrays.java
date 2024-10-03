f726e929662f847211dfa0ea7fba900a78ac8f78
Emircan <emircanturan010@gmail.com>
2024-10-04 00:07:57 +0300

    Day 04

---
+++ src/main/java/day04/C01_arrays.java
@@ -0,0 +1,19 @@
+package day04;
+
+import java.util.Arrays;
+
+public class C01_arrays {
+    public static void main(String[] args) {
+
+        /*
+        1.soru bir array olusturun ve olusturdugunuz arraydeki
+        tum elemanları yazdırın
+        */
+
+        String[] isimler = {"Züleyha", "Ummuhan", "Alper", "Melek", "Yuşa", "Neslihan", "Ali Can"};
+        System.out.println(Arrays.toString(isimler)); // Arrays classı yardımcı class
+        for (String s : isimler) {
+            System.out.println(s);
+        }
+    }
+}
